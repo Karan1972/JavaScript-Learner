08f442b6260bee023663f275061a0eff549977e6
Karan Varshney <[karanvarshney071@gmail.com]>
2024-08-15 12:47:39 +0530

    datatypes

---
+++ 01_basics/03_Datatypes.js
@@ -0,0 +1,21 @@
+const array = [ "Google" , "Microsoft" , "ZSassociates" , "Amazon" , "Optum"];
+
+ //console.log(array);
+
+ const obj = {
+
+    "karan" : 22,
+    "varun" : 24,
+    "krishna" : 12
+ }
+
+ console.log(obj["karan"]);
+
+ const myfunc = function(){
+
+    console.log("vdia function");
+    
+ }
+ console.log(myfunc());
+ 
+ 
