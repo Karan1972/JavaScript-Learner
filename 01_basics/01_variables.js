fb2bbff93ddd0d1839a8a5a2c780055001cec90a
Karan Varshney <[karanvarshney071@gmail.com]>
2024-08-15 20:24:27 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+const firstName = "Karan"
+let lastName = "Varsheny"
+var otherName = "nicky"
+
+namer = "hridoy"
+
+//console.log(firstName);
+
+otherName = "Asalanka"
+
+lastName = "Padukone"
+//console.log(lastName);
+
+
+if (otherName === "Asalanka"){
+
+    otherName = "FriedRich"
+}
+
+if (lastName === "Padukone"){
+
+    lastName = "Watson"
+}
+
+console.log(otherName);
+console.log(lastName);
+
+
+
+
+
