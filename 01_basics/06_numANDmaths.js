8bd6bb2c20bc4908db47b6c531d73dc9e7300cbf
Karan Varshney <[karanvarshney071@gmail.com]>
2024-08-17 22:18:24 +0530

    updated

--- 01_basics/06_numANDmaths.js
+++ 01_basics/06_numANDmaths.js
@@ -1,30 +1,30 @@
-// const value = 56645677
+const value = 56645677
 
-// //  number to string
+//  number to string
 
-// const stringValue = value.toString();
+const stringValue = value.toString();
 
-// console.log(stringValue.charAt(4));
+console.log(stringValue.charAt(4));
 
-// const value2 = 123.76769898
+const value2 = 123.76769898
 
-// console.log(value2.toPrecision(5));            // estimate over all value from the starting , no role of decimal 
-// console.log(value2.toFixed(3));               // estimate value over 3 decimal places 
+console.log(value2.toPrecision(5));            // estimate over all value from the starting , no role of decimal 
+console.log(value2.toFixed(3));               // estimate value over 3 decimal places 
 
-// const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
-// console.log(hundreds.toLocaleString());
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString());
 
 
 
-// ++++++++++++++++++++++++++  Maths +++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++  Maths +++++++++++++++++++++++++++++++++++++++++++++++
 
-// console.log(Math);
-// console.log(Math.abs(-67));
-// console.log(Math.round(4.4999999));
-// console.log(Math.ceil(4.333));
-// console.log(Math.floor(4.99));
-// console.log(Math.max(3, 4, 4, 5.5 , 6.66 ));
+console.log(Math);
+console.log(Math.abs(-67));
+console.log(Math.round(4.4999999));
+console.log(Math.ceil(4.333));
+console.log(Math.floor(4.99));
+console.log(Math.max(3, 4, 4, 5.5 , 6.66 ));
 
 console.log( Math.random());
 
